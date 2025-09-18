74a32833c51bc9e27a50cf4e7466f6cbcd38ecbf
Chandrashekhar Bhujbal <chandrashekhar@Chandrashekhars-Mac-Studio.local>
2025-09-18 19:46:23 +0530

    chore: minimal working Next.js app router + config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // TEMP for MVP: don't block builds on types/eslint (you can re-enable later)
+  // Relax checks for now so build doesn't fail on types/lints
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  eslint: { ignoreDuringBuilds: true }
 };
 module.exports = nextConfig;
